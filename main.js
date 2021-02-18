b725e76bf6b30d7a1bf3b7347e02071b95b74ff0
Michał Marczuk <michal.marczuk@globalapptesting.com>
2021-02-18 13:11:46 +0100

    Added sample request

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import axios from 'axios';
+
+class Test {
+    constructor() {
+        this.instance = axios.create({
+            baseURL: 'http://localhost:8000',
+            timeout: 1000,
+            headers: { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5pbHNvbkBlbWFpbC5jb20iLCJwYXNzd29yZCI6Im5pbHNvbiIsImlhdCI6MTYxMzY1MDE5MiwiZXhwIjoxNjEzNjUzNzkyfQ.FlcPfWT0r9ruQhH9XWccL9Udnx3NYKGn4VivN9XRjIQ' },
+            validateStatus: () => true,
+        });
+    }
+
+    async me() {
+        const response = await this.instance.get('people');
+        console.log(response.data);
+    }
+}
+
+const test = new Test();
+test.me().then();
