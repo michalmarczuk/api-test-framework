ed28e922d20cd20573155d1021296e6e2a59c9f5
Michal Marczuk <MMarczu@rockwellautomation.com>
2021-02-25 09:11:13 +0100

    Added parametrized test for failure login

--- __tests__/login.spec.js
+++ __tests__/login.spec.js
@@ -1,11 +1,26 @@
-import Test from '../main.js'
+import ApiClient from '../apiClient.js'
 
-describe("Login function", () => {
-    test("Failure login", async () => {
-        const test = new Test();
-        const peopleData = (await test.people()).data;
-        console.log(peopleData);
+describe('Login feature', () => {
+    test('People resource without authorization', async () => {
+        const apiClient = new ApiClient();
+        const peopleData = (await apiClient.people()).data;
 
-        expect(peopleData.status).toEqual(404);
+        expect(peopleData.status).toEqual(401);
     });
+
+    test.each([
+        ['', ''],
+        ['invalid_login', 'invalid_password'],
+    ])('Failure login: [login: "%s"][password: "%s"]', async (login, password) => {
+        const apiClient = new ApiClient();
+        const loginResponse = (await apiClient.login(login, password));
+        
+        const expectedData = {
+            "status": 401,
+            "message": "Incorrect email or password"
+        }
+        expect(loginResponse.status).toEqual(401);
+        expect(loginResponse.data).toEqual(expectedData);
+    });
+
 });
--- apiClient.js
+++ apiClient.js
@@ -2,9 +2,9 @@
 
 import axios from 'axios';
 
-export default class Test {
+export default class ApiClient {
     constructor() {
-        this.instance = axios.create({
+        this.axiosInstance = axios.create({
             baseURL: 'http://localhost:8000',
             timeout: 1000,
             headers: { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5pbHNvbkBlbWFpbC5jb20iLCJwYXNzd29yZCI6Im5pbHNvbiIsImlhdCI6MTYxMzY1MDE5MiwiZXhwIjoxNjEzNjUzNzkyfQ.FlcPfWT0r9ruQhH9XWccL9Udnx3NYKGn4VivN9XRjIQ' },
@@ -12,7 +12,11 @@ export default class Test {
         });
     }
 
+    async login(user = 'bruno@email.com', password = 'bruno') {
+        return await this.axiosInstance.post('auth/login');
+    }
+
     async people() {
-        return await this.instance.get('people');
+        return await this.axiosInstance.get('people');
     }
 }
