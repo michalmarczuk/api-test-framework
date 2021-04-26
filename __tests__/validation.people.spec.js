93691bbf827ed278a42273a9a19d93766265b28e
Michał Marczuk <michal.marczuk@globalapptesting.com>
2021-04-26 10:08:31 +0200

    Refactored response validation part 4

--- __tests__/validation.people.spec.js
+++ __tests__/validation.people.spec.js
@@ -1,69 +1,57 @@
-import ApiClient from '../lib/apiClient.js'
+import ApiClient from '../lib/apiClient.js';
 import People from '../lib/endpoints/people/people.js';
 
-describe('Response validation for people resource', () => {
-    test('[Get] Invalid type', async() => {
-        const apiClient = new ApiClient();
-        await apiClient.login();
 
+describe('Response validation for people resource', () => {
+    let apiClient;
+    const validateErrorMessage = (validationErrors, expectedErrorMessage) => {
+        expect(validationErrors[0].message).toEqual(expectedErrorMessage);
+    }
+
+    beforeEach(async () => {
+        apiClient = new ApiClient();
+        await apiClient.login();
+    });
+
+    test('[Get] Invalid type', async() => {
         const peopleResponse = await apiClient.people().get('6044ee2a23f888686e06c0eb');
         peopleResponse.data.age = '30';
 
-        const validationErrors = People.validateGetResponse(peopleResponse.data).errors;
-        expect(validationErrors[0].message).toEqual('is not of a type(s) integer');
+        validateErrorMessage(People.validateGetResponse(peopleResponse.data).errors, 'is not of a type(s) integer');
     });
 
     test('[Get] Additional param', async() => {
-        const apiClient = new ApiClient();
-        await apiClient.login();
-
         const peopleResponse = await apiClient.people().get('6044ee2a23f888686e06c0eb');
         peopleResponse.data.newParam = '123';
 
-        const validationErrors = People.validateGetResponse(peopleResponse.data).errors;
-        expect(validationErrors[0].message).toEqual('is not allowed to have the additional property "newParam"');
+        validateErrorMessage(People.validateGetResponse(peopleResponse.data).errors, 'is not allowed to have the additional property "newParam"');
     });
 
     test('[Get] Lack of required param', async() => {
-        const apiClient = new ApiClient();
-        await apiClient.login();
-
         const peopleResponse = await apiClient.people().get('6044ee2a23f888686e06c0eb');
         delete peopleResponse.data.company;
 
-        const validationErrors = People.validateGetResponse(peopleResponse.data).errors;
-        expect(validationErrors[0].message).toEqual('requires property "company"');
+        validateErrorMessage(People.validateGetResponse(peopleResponse.data).errors, 'requires property "company"');
     });
-    test('[Get All] Invalid type', async() => {
-        const apiClient = new ApiClient();
-        await apiClient.login();
 
+    test('[Get All] Invalid type', async() => {
         const peopleResponse = await apiClient.people().get();
         peopleResponse.data[22].age = '30';
 
-        const validationErrors = People.validateGetAllResponse(peopleResponse.data).errors;
-        expect(validationErrors[0].message).toEqual('is not of a type(s) integer');
+        validateErrorMessage(People.validateGetAllResponse(peopleResponse.data).errors, 'is not of a type(s) integer');
     });
 
     test('[Get All] Additional param', async() => {
-        const apiClient = new ApiClient();
-        await apiClient.login();
-
         const peopleResponse = await apiClient.people().get();
         peopleResponse.data[22].newParam = '123';
 
-        const validationErrors = People.validateGetAllResponse(peopleResponse.data).errors;
-        expect(validationErrors[0].message).toEqual('is not allowed to have the additional property "newParam"');
+        validateErrorMessage(People.validateGetAllResponse(peopleResponse.data).errors, 'is not allowed to have the additional property "newParam"');
     });
 
     test('[Get All] Lack of required param', async() => {
-        const apiClient = new ApiClient();
-        await apiClient.login();
-
         const peopleResponse = await apiClient.people().get();
         delete peopleResponse.data[22].company;
 
-        const validationErrors = People.validateGetAllResponse(peopleResponse.data).errors;
-        expect(validationErrors[0].message).toEqual('requires property "company"');
+        validateErrorMessage(People.validateGetAllResponse(peopleResponse.data).errors, 'requires property "company"');
     });
 });
